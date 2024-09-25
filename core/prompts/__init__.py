71612734d173bf44b37645d6e7c56a584602aa61
tikendraw <tikendraksahu1029@gmail.com>
2024-09-25 20:07:37 +0530

    init

--- core/prompts/__init__.py
+++ core/prompts/__init__.py
@@ -1,2 +1,15 @@
 
+from .cot import REVIEW_PROMPT, SYSTEM_PROMPT ,FINAL_ANSWER_PROMPT
+from .chat_mark_chat import SYSTEM_PROMPT_FOR_CHAT
+from .decision_prompt import COT_OR_DA_PROMPT
+from .general import HELPFUL_ASSISTANT_PROMPT
 
+
+__all__ = [
+    "COT_OR_DA_PROMPT",
+    "REVIEW_PROMPT",
+    "SYSTEM_PROMPT",
+    "FINAL_ANSWER_PROMPT",
+    "SYSTEM_PROMPT_FOR_CHAT",
+    "HELPFUL_ASSISTANT_PROMPT"
+]
